a82acd898a87d05d685dec4364b56e54467363e3
Program <ProgramCX@Outlook.com>
2024-11-04 22:30:28 +0800

    feat: import necessary modules

--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//import ElementPlus module
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css' 
+
+//import ElementPlusIconVue
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+//import Router
+import {Router} from './tools/Router'
+const app= createApp(App).mount('#app')
+
+//mount ElementPlus module
+app.use(ElementPlus)
+
+//mount Router module
+app.use(Router)
+//regist all icons in ElementPlusVue
+for(const [key,component] of Object.entries(ElementPlusIconsVue)){
+    app.component(key,component)
+}
---
+++ src/tools/Router.js
@@ -0,0 +1,40 @@
+﻿import { createRouter, createWebHashHistory } from "vue-router";
+
+import App from "@/App.vue";
+
+
+import NewFeature from "@/components/NewFeature.vue";
+import ProductDisplay from "@/components/ProductDisplay.vue";
+import ReleaseDownload from "@/components/ReleaseDownload.vue";
+import ContributeComp from "@/components/ContributeComp.vue";
+
+const Router=createRouter({
+    history:createWebHashHistory(),
+    routes:[
+        {
+            path:'/index',
+            component:App,
+            name:'index'
+        },
+        {
+            path:'/newfeature',
+            component:NewFeature,
+            name:'newfeature'
+        },
+        {
+            path:'/productdisplay',
+            component:ProductDisplay,
+            name:'productdisplay'
+        },{
+            path:'releasedownload',
+            component:ReleaseDownload,
+            name:'releasedownload'
+        },{
+            path:'contributecomp',
+            component:ContributeComp,
+            name:'contributecomp'
+        }
+    ]
+})
+
+export default Router;
