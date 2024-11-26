5577161733119984056760cb94f5a529725b12e1
Program <ProgramCX@Outlook.com>
2024-11-26 22:39:22 +0800

    chore: add regist info

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -2,6 +2,7 @@
     <el-footer class="footer">
         <p style="font-size:13px">© 2024 程旭 版权所有</p>
         <a href="https://beian.miit.gov.cn/" target="_blank">皖ICP备2024067408号</a>
+        <span id="regist"><img src="../assets/img/regist.png" height="13px" style="margin-right: 5px;vertical-align:middle"/><a href="https://beian.mps.gov.cn/#/query/webSearch?code=34118102000926" rel="noreferrer" target="_blank">皖公网安备34118102000926</a></span>
     </el-footer>
 </template>
 
@@ -23,13 +24,17 @@
     bottom:0;
     /* text-align: center; */
 }
-.footer>a{
+.footer a{
     color: #dbdbdb;
     text-decoration: none;
-    font-size: 13px
+    font-size: 13px;
+    line-height: 30px;
 }
-.footer>a:hover{
+.footer a:hover{
     color: white;
     transition: color,1s;
+}
+#regist{
+    margin-left: 30px;
 }
 </style>
